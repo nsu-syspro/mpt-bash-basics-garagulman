284f2763c2faf43a755ae5995001cc5a019030d8
garagulman <akgaragulova@gmail.com>
2025-09-07 09:44:28 +0700

    fizzbuzz

--- solution/fizzbuzz.sh
+++ solution/fizzbuzz.sh
@@ -1,14 +1,14 @@
-N=$1
+#!/usr/bin/env bash
 i=1
-while [ $i -le $N ]; do
-  if ! (( $i % 3 )) && (( $i % 5 )) ; then
+while [ $i -le "$1" ]; do
+  if ! (( i % 3 )) && (( i % 5 )) ; then
     echo "Fizz"
-  elif ! (( $i % 5 )) && (( $i % 3 )) ; then
+  elif ! (( i % 5 )) && (( i % 3 )) ; then
     echo "Buzz"
-  elif ! (( $i % 3 )) && ! (( $i % 5)) ; then
+  elif ! (( i % 3 )) && ! (( i % 5)) ; then
     echo "Fizz Buzz"
   else 
     echo "$i"
   fi
-  i=$(( $i +1 ))
+  i=$(( i +1 ))
 done
